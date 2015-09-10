3bb0b1c4a95d79df1097485348bfb1c64897598c
zoenberger <jeflux@gmail.com>
2015-09-10 10:40:48 -0700

    Initial Commit
    Includes min copy of jquery

---
+++ RandColor.js
@@ -0,0 +1,42 @@
+// JavaScript source code
+
+$(document).ready(function () {
+    newColor = getColor();
+    $("ul li h1").css("background-color", newColor);
+    $("ul li h1").text("Current Color: " + newColor);
+
+
+    $("#changeColor").click(function () {
+        newColor = getColor();
+        oldColor = rgb2hex($("h1").css('backgroundColor'));
+        oldColor = oldColor.toUpperCase();
+        //$("#colors").append("<li style='background-color:" + oldColor + "'>" + oldColor + "</li>");
+        $("li:nth-child(1)").after("<li style='background-color:"+oldColor+"'>" + oldColor + "</li>");
+        $("ul li h1").css("background-color", newColor);
+        $("h1").text("Current Color: " + newColor);
+    });
+
+});
+
+
+function getColor() {
+    var newColorText = "#";
+    var hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+
+    for (i = 0; i < 6; i++)   {
+        newColorText = newColorText + hex[Math.floor(Math.random() * 16)];
+    }
+
+   
+    return newColorText;
+}
+
+
+//Function to convert hex format to a rgb color
+function rgb2hex(orig) {
+    var rgb = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+)/i);
+    return (rgb && rgb.length === 4) ? "#" +
+     ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+     ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+     ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : orig;
+}
